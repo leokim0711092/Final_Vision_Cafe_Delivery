a80f24c7e450ce51553861f151f17e3b67cf9182
leokim0711092 <leokim10073@gmail.com>
2024-02-23 11:18:37 +0000

    Modify the launch file to correct folder name

---
+++ moveit2_scripts/launch/pick_and_place.launch.py
@@ -0,0 +1,43 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("name", package_name="moveit_config_sim").to_moveit_configs()
+    # moveit_config = MoveItConfigsBuilder("my").to_moveit_configs()
+
+    # MoveItCpp demo executable
+    moveit_cpp_node = Node(
+        name="pick_and_place",
+        package="moveit2_scripts",
+        executable="pick_and_place",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            {'use_sim_time': True},
+        ],
+    )
+
+    # MoveItCpp demo executable
+    moveit_scene = Node(
+        name="scene",
+        package="moveit2_scripts",
+        executable="scene",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            {'use_sim_time': True},
+        ],
+    )
+
+    return LaunchDescription(
+        [moveit_cpp_node, moveit_scene]  
+    )
